0996adccf9f37108357ab9aa28dec474c7d8fda0
Jürg Messmer <jm@mosaics.ch>
2012-10-05 16:56:25 +0200

    initial init

---
+++ README.md
@@ -0,0 +1,46 @@
+# Sitemap-Plugin
+This plugin is a plugin that works seamlessly with Rapila CMS
+It includes
+* FileModule XMLSitemap 
+	to generate of a XML document compatiple with http://www.sitemaps.org/protocol.html
+* Html Sitemap Example 
+	tips on how to implement a html sitemap in your Rapila site
+
+
+## FileModule XMLSitemap
+i.e. for search engines like Google, can be included in the robots.txt
+
+md~~~
+Sitemap: http://www.yourdomain.com/get_file/xml_sitemap
+~~~
+
+## Html Sitemap Example
+This example requires only a sitemap.tmpl and a navigation configuration specified for the sitemap
+You can choose whether you want to 
+* show hidden pages/navigation item
+* hide duplicates of canonical pages
+* the number of levels you want to display
+* the html and css of the rendered items by defining your own inline templates
+
+An example of a typical sitemap navigation
+yaml~~~
+navigations:
+  sitemap:
+    exclude_duplicates: true
+    show_hidden: true
+    0:
+      - {template_inline: '<ul><li><a href="{{link}}">{{title}}</a><ul>{{children}}</ul></li></ul>'}
+    all:
+      - {template_inline: '<li class="level_{{level}}"><a class="{{name}}" href="{{link}}">{{title}}</a><ul>{{children}}</ul></li>'}
+    4:
+      - show: false
+~~~
+
+Example of how to configure the sitemap navigation in the template
+html~~~
+	<body class="sitemap">
+		<div>
+			{{navigation=sitemap}}
+		</div>
+  </body>
+~~~
---
+++ modules/file/xml_sitemap/XmlSitemapFileModule.php
@@ -0,0 +1,60 @@
+<?php
+class XmlSitemapFileModule extends FileModule {
+
+	private $sXmlNamespace = 'http://www.sitemaps.org/schemas/sitemap/0.9';
+	private $oXmlDocument;
+	private $oUrl;
+	
+	public function __construct($aRequestPath) {
+		parent::__construct($aRequestPath);
+	}
+	
+	private function element($sName, $sContent) {
+		$oUrl = $this->oXmlDocument->createElement($sName);
+		$oUrl->appendChild($this->oXmlDocument->createTextNode($sContent));
+		return $oUrl;
+	}
+	
+	private function renderNavigationItem($oNavigationItem) {
+		$oUrl = $this->oXmlDocument->createElement('url');
+		
+		//Location, absolute url
+		$sAbsoluteLink = LinkUtil::absoluteLink(LinkUtil::link($oNavigationItem->getLink(), 'FrontendManager'));
+		$oUrl->appendChild($this->element('loc', $sAbsoluteLink));
+
+		//Last modified
+		if($oNavigationItem instanceof PageNavigationItem) {
+			$oUrl->appendChild($this->element('lastmod', $oNavigationItem->getMe()->getUpdatedAt('Y-m-d')));
+		} else {
+			//@todo: figure out how to retrieve last modified for content other then text and configuration: FrontendModules
+		}
+		
+		//Priority
+		$iPriority = 1;
+		if($oNavigationItem->getCanonical() !== null) {
+			$iPriority = 0.5;
+		}
+		$oUrl->appendChild($this->element('priority', $iPriority));
+		
+		$this->oUrl->appendChild($oUrl);
+		
+		foreach($oNavigationItem->getChildren(null, false, true) as $oChild) {
+			$this->renderNavigationItem($oChild);
+		}
+	}
+	
+	public function renderFile() {
+		$this->oXmlDocument = new DOMDocument();
+		$this->oUrl = $this->oXmlDocument->createElement('urlset');
+		$this->oUrl->setAttribute('xmlns', $this->sXmlNamespace);
+		$this->oXmlDocument->appendChild($this->oUrl);
+		
+		$oRootPage = PagePeer::getRootPage();
+		$oRootNavigationItem = PageNavigationItem::navigationItemForPage($oRootPage);
+		
+		$this->renderNavigationItem($oRootNavigationItem);
+
+		header('Content-Type: application/xml;charset=utf-8');
+		print $this->oXmlDocument->saveXML();
+	}
+}
